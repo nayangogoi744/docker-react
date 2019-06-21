ab20526f64010521db741ce111b49d8362601039
root <root@rhelplatform.eur.ad.sag>
2019-06-21 15:28:39 +0530

    change app test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Nayan, how are you?Good!
+          Hello Nayan, I have been updated!
         </p>
         <a
           className="App-link"
